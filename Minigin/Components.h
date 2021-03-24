819db57161cdbeb66cde5a6ec5dd7aea29229cac
kdedeurw <66272745+kdedeurw@users.noreply.github.com>
2021-03-24 22:21:39 +0100

    !MOVED to Engine!
    -Deleted outdated 'Minigin' folder

--- Minigin/Components.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "TextComponent.h"
-#include "FPSComponent.h"
-#include "Texture2DComponent.h"
-#include "FPSComponent.h"
-#include "SpriteComponent.h"
-#include "PhysicsComponent.h"
--- Minigin/Globals.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "GameState.h"
-#include "GlobalInput.h"
-#include "GlobalMemoryPools.h"
-#include "SceneManager.h"
-#include "Renderer.h"
-#include "ResourceManager.h"
